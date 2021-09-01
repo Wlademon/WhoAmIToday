97c2613e32e05e1cc29c3e0faa53c873570c8433
WLAdemon <wladoseid@gmail.com>
2021-09-02 00:37:06 +0300

    Version 1.01 - Расширен список выражений - Добавлены предсказания - Исправлен баг с рандомом - Добавлена команда /state - Список выражений перенесен из .env в vars.yml

--- main.go
+++ main.go
@@ -1,41 +1,89 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"math/rand"
 	"os"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/Wlademon/vkBot/file"
 	"github.com/Wlademon/vkBot/file/cache"
 	absTime "github.com/Wlademon/vkBot/time"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/goccy/go-yaml"
 	"github.com/joho/godotenv"
 )
 
 var Location *time.Location
+
 var TodayNames []string
+var TodayPrediction []string
+
+var Peoples map[int64]struct {
+	Link  string
+	Name  string
+	Date  string
+	Value string
+}
 
 func main() {
 	cache.InitCache("cache")
 	initEnv()
-	setTodayNames()
+	setVars()
+	getCacheStat()
 	absTime.InitTime("Europe/Moscow")
 	Location = time.FixedZone("Current", int(time.Hour*3/time.Second))
+	Peoples = make(map[int64]struct {
+		Link  string
+		Name  string
+		Date  string
+		Value string
+	})
 	bot, err := initBot()
 	if err != nil {
 		panic(err)
 	}
-
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 	updates, _ := bot.GetUpdatesChan(u)
-
 	observeCommands(updates, bot)
 }
 
-func setTodayNames() {
-	TodayNames = strings.Split(os.Getenv("VARIABLES"), "%%%")
+func setCacheStat() {
+	marshal, err := json.Marshal(Peoples)
+	if err != nil {
+		return
+	}
+	cache.CreateForever("PEOPLES", string(marshal)).Set()
+}
+
+func getCacheStat() {
+	ex, val := cache.Get("PEOPLES")
+	if ex {
+		err := json.Unmarshal([]byte(val), &Peoples)
+		if err != nil {
+			return
+		}
+	}
+}
+
+func setVars() {
+	yamlFile := file.File{Name: "vars.yml"}
+	read, err := yamlFile.Read()
+	if err != nil {
+		return
+	}
+	y := make(map[string][]string)
+
+	err = yaml.Unmarshal(read, &y)
+	if err != nil {
+		return
+	}
+	TodayNames = y["variables"]
+	TodayPrediction = y["prediction"]
 }
 
 func getTodayName(user int64) string {
@@ -51,20 +99,24 @@ func getTodayName(user int64) string {
 	if value == "" {
 		return "Вытяни другую"
 	}
-	value = "Сегодня ты: " + value
 	cache.Create(strconv.FormatInt(user, 10), value, nextDate.Sub(nowDate)).Set()
 
 	return value
 }
 
 func randName(user int64) string {
-	rand.Seed(time.Now().Unix())
-	i := rand.Intn(len(TodayNames) + 1)
-	if i > len(TodayNames)-1 {
+	rand.Seed(time.Now().UnixNano())
+	i := rand.Intn(len(TodayNames) - 1)
+	variable := TodayNames[i]
+	if variable == "@" {
+		pI := rand.Intn(len(TodayPrediction) - 1)
+		return "Тебе выпало предсказание: " + TodayPrediction[pI]
+	}
+	if variable == "+" {
 		return ""
 	}
 
-	return TodayNames[i]
+	return "Сегодня ты: " + TodayNames[i]
 }
 
 func initEnv() {
@@ -118,13 +170,27 @@ func runCommand(command string, message *tgbotapi.Message) (string, bool) {
 	arrCommand := strings.Split(strings.Trim(strings.ReplaceAll(command, "  ", " "), " "), " ")
 	commandExec := arrCommand[0]
 	commandExec = strings.Split(commandExec, "@")[0]
-
 	switch commandExec {
+	case "/state":
+		result := ""
+		for id, p := range Peoples {
+			result +=
+				"\n--------------------\n" +
+					fmt.Sprintf("ID: %d\nName: %s\nLink: @%s\nValue: %s\nDate: %s", id, p.Name, p.Link, p.Value, p.Date)
+		}
+		return result, false
 	case "/reset_names":
-		setTodayNames()
+		setVars()
 		return "OK", true
 	case "/who_am_i_today":
-		return getTodayName(user), true
+		Peoples[int64(message.From.ID)] = struct {
+			Link  string
+			Name  string
+			Date  string
+			Value string
+		}{Link: message.From.String(), Name: message.From.FirstName + " " + message.From.LastName, Date: time.Now().In(Location).Format("2006-01-02"), Value: getTodayName(user)}
+		setCacheStat()
+		return Peoples[int64(message.From.ID)].Value, true
 	}
 
 	return "", false
